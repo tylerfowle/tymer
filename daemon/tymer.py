ce9c095dcf00dccc92ca30bade2d7adec72f89b9
tylerfowle <tylerfowle@gmail.com>
2017-12-08 15:18:59 -0600

    start rewrite in python

--- daemon/tymer.py
+++ daemon/tymer.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python
+import sys
+import os
+import datetime
+import time
+from time import sleep
 
-print "Hello World"
+print (sys.version)
+
+TYMERS_FILEPATH=os.path.expanduser('~/.tymer/tymers.txt')
+
+TYMERS_FILE = open(TYMERS_FILEPATH, "r")
+
+CURRENT_TIME=time.ctime()
+CURRENT_TIME_SEC=time.time()
+
+print(CURRENT_TIME)
+print(CURRENT_TIME_SEC)
+sleep(10)
+while True:
+    CURRENT_TIME=time.ctime()
+    CURRENT_TIME_SEC=time.time()
+    os.system('clear')
+    print(CURRENT_TIME_SEC)
+    sleep(1)
+
+
